7e3a5f37b4e5b97530c8e5f6be8c5e615e231e87
ilhamhafizha <muhammadilhamhafizha@gmail.com>
2025-07-03 15:21:41 +0700

    feat: Implement Token Authorization

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
+function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Unauthorized: Token missing' });
+    return res.status(401).json({ message: 'Unauthorized: No token provided' });
   }
 
   const token = authHeader.split(' ')[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    req.user = decoded; // bisa digunakan di handler nanti
     next();
   } catch (error) {
-    return res.status(403).json({ message: 'Forbidden: Invalid token' });
+    return res.status(403).json({ message: 'Invalid or expired token' });
   }
-};
+}
 
 module.exports = verifyToken;
--- routes/todos.route.js
+++ routes/todos.route.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 const handler = require('../handlers/todos.handler');
 const validate = require('../middlewares/todos.validate');
+const verifyToken = require('../middlewares/auth.middleware');
 const { todoSchema } = require('../validators/todo.validator');
 
-router.get('/', handler.getTodos);
-router.get('/:id', handler.getTodoById);
-// router.post('/', handler.createTodo);
-// router.put('/:id', handler.updateTodo);
-router.post('/', validate(todoSchema), handler.createTodo);
-router.put('/:id', validate(todoSchema), handler.updateTodo);
-router.delete('/:id', handler.deleteTodo);
+// Semua rute di bawah ini akan dicek token-nya dulu
+router.get('/', verifyToken, handler.getTodos);
+router.get('/:id', verifyToken, handler.getTodoById);
+router.post('/', verifyToken, validate(todoSchema), handler.createTodo);
+router.put('/:id', verifyToken, validate(todoSchema), handler.updateTodo);
+router.delete('/:id', verifyToken, handler.deleteTodo);
 
 module.exports = router;
